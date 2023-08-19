2d5d1ad525821c36a86dbdb11a6fd27bc115f77e
SuryaAssistant <09.fandisurya@gmail.com>
2023-08-20 00:22:05 +0700

    feat: add message filtering based on JSON key

--- src/armv7l/main.py
+++ src/armv7l/main.py
@@ -101,14 +101,53 @@ def ECDSA_begin():
         f.close()
 
 #=======================================================================
+# Function to select operator as filter
+# Parameter:
+# - variable : value in JSON
+# - parameter_value : value from user
+#=======================================================================
+def compare_greater(variable, value):
+    return variable > value
+
+def compare_less(variable, value):
+    return variable < value
+
+def compare_equal(variable, value):
+    return variable == value
+
+def compare_greater_equal(variable, value):
+    return variable >= value
+
+def compare_less_equal(variable, value):
+    return variable <= value
+
+def compare_not_equal(variable, value):
+    return variable != value
+
+operator_map = {
+    '>':compare_greater,
+    '<':compare_less,
+    '==': compare_equal,
+    '>=': compare_greater_equal,
+    '<=': compare_less_equal,
+    '!=': compare_not_equal,
+}
+
+#=======================================================================
 # Function to act based on input command in API
 # Parameter:
 # - command : command to do
 # - parameter_value : value to input in command
 # - return_topic : topic used to send MQTT
 #=======================================================================
-def do_command(command, parameter_value, return_topic, set_tag=gateway_name):
-    # convert compressed public key to PEM format
+def do_command(full_input_command):
+    parsing_data = full_input_command.split('/')
+    command = parsing_data[0]
+    parameter_value = parsing_data[1]
+    return_topic = parsing_data[2].replace("'", "")
+
+    # Convert compressed public key to PEM format
+    # Format: convert_to_pem/<compressedPublicKey>/<return_topic>
     if command == 'convert_to_pem':
         try :
             compressedPublicKey = parameter_value
@@ -117,72 +156,180 @@ def do_command(command, parameter_value, return_topic, set_tag=gateway_name):
             send_mqtt(publicKey_pem, return_topic)
         except ValueError :
             send_mqtt("Error to convert compressed public key to PEM format", return_topic)
+        except :
+            send_mqtt("Unknown error", return_topic)
             
-    # get data section of a message
-    if command == 'data':
+    # Upload data to tangle
+    # Format: data/<parameter_value>/<return_topic>/<specified_tag_index>
+    elif command == 'data':
         try :
             parameter_value = parameter_value.replace("'", '"')
-            upload(parameter_value, gateway_name)
+            tag_index = parsing_data[3]
+            upload(parameter_value, tag_index)
             send_mqtt(tangle_msg_id, return_topic)
         except ValueError :
             send_mqtt("Error to upload to Tangle", return_topic)
+        except IndexError :
+            send_mqtt("Format command not found", return_topic)
+        except :
+            send_mqtt("Unknown error", return_topic)
             
-    # Upload data with specified tag index
-    elif command == 'data_special':
-        try :
-            parameter_value = parameter_value.replace("'", '"')
-            upload(parameter_value, set_tag)
-            send_mqtt(tangle_msg_id, return_topic)
-        except ValueError :
-            send_mqtt("Error to upload to Tangle", return_topic)
-            
-    # get list of message_id based on indexation name
+    # Get list of message_id based on indexation name
+    # Format: tag/<tag_index>/<return_topic>
     elif command == 'tag':
         try :
             return_data = str(client.get_message_index(parameter_value))
         except ValueError :
             return_data = "Tag not found"
+        except :
+            return_data = "Unknown Error"
+
         send_mqtt(return_data, return_topic)
 
     # Original data from IOTA Tangle
+    # Format: msg_data/<msg_id>/<return_topic>
     elif command == 'msg_data':
         try : 
             return_data = str(client.get_message_data(parameter_value))
         except ValueError:
             return_data = "Message ID not found"
+        except :
+            return_data = "Unknown error"
+
         send_mqtt(return_data, return_topic)
             
-    # original metadata from IOTA Tangle
+    # Original metadata from IOTA Tangle
+    # Format: msg_metadata/<msg_id>/<return_topic>
     elif command == 'msg_metadata':
         try:
             return_data = str(client.get_message_metadata(parameter_value))
         except ValueError:
             return_data = "Message ID not found"
+        except :
+            return_data = "Unknown error"
+
         send_mqtt(return_data, return_topic)
         
-    # get list of message in tag index
+    # Get list of message in tag index
+    # Support non JSON compatible data
+    # Format: tag_msg/<tag_index>/<return_topic>
     elif command == 'tag_msg':
         try :
             # get list of 
             msg_id_list= client.get_message_index(parameter_value)
-            msg_count = len(msg_id_list)
             return_data = "["
             
             # get payload for every message ID
-            for i in range(msg_count):
+            for i in range(len(msg_id_list)):
                 full_data = client.get_message_data(msg_id_list[i]) 
                 payload_byte = full_data["payload"]["indexation"][0]["data"]
                 msg=''
                 for x in range(len(payload_byte)):
                     msg += chr(payload_byte[x])
                 return_data += "[{'msgID':'" + msg_id_list[i] + "'}," + msg + "]"
-                if i < msg_count-1:
+                if i < len(msg_id_list)-1:
                     return_data += ","
             
             return_data += "]"
             return_data = return_data.replace('"', "'")
         except ValueError :
             return_data = "Tag not found"
+        except :
+            return_data = "Unknown error"
+            
+        send_mqtt(return_data, return_topic)
+
+    # Get list of filtered message in tag index
+    # Only support JSON compatible data
+    # Format: tag_msg_filter/<tag_index>/<return_topic>/<operator:value>/position
+    elif command == 'tag_msg_filter':
+        try :
+            # get list of msg id in index
+            msg_id_list= client.get_message_index(parameter_value)
+            return_data = "["
+            
+            # get payload for every message ID
+            for i in range(len(msg_id_list)):
+                full_data = client.get_message_data(msg_id_list[i]) 
+                payload_byte = full_data["payload"]["indexation"][0]["data"]
+                msg=''
+                for x in range(len(payload_byte)):
+                    msg += chr(payload_byte[x])
+
+                # create object JSON. Only valid JSON object is acceptable
+                # if data from blockchain not JSON already, it will except
+                try:
+                    object_json = json.loads(msg)
+                except ValueError:
+                    print("Data Error: Received data is not in JSON compatible")
+                    continue
+
+                # get operator and value
+                operator, user_value = parsing_data[3].split(':')
+                parse_count = len(parsing_data)
+
+                # create JSON parent-child key
+                key_position = ['master']
+                for x in range(0, parse_count-4):
+                    try: 
+                        key_position[x] = parsing_data[x+4]
+                    except IndexError:
+                        key_position.append(parsing_data[x+4])
+                
+                # if key is not found in this try, 
+                # go to next data
+                try:
+                    # compare key_value and user_value
+                    if operator in operator_map:
+                        comparison_func = operator_map[operator]
+                        key_value=''
+                        
+                        for x in range(len(key_position)):
+                            # determine key is string or int
+                            key = key_position[x]
+                            if '"' in key or "'" in key:
+                                key = key.replace("'", "")
+                                key = key.replace('"', '')
+                            else:
+                                key = int(key)
+
+                            # Get value from key                            
+                            if x==0:
+                                key_value = object_json[key]
+                            else:
+                                key_value = key_value[key]
+                                            
+                        # determine user_value is string or int
+                        if '"' in user_value or "'" in user_value:
+                            user_value = str(user_value)
+                            user_value = user_value.replace("'", "")
+                            user_value = user_value.replace('"', '')
+                        else:
+                            user_value = int(user_value)
+
+                        # compare both values
+                        result = comparison_func(key_value, user_value)
+
+                        # if comparison is satisfied, add to return_data 
+                        if result:
+                            return_data += "[{'msgID':'" + msg_id_list[i] + "'}," + msg + "]"
+                            if i < len(msg_id_list)-1:
+                                return_data += ","
+                except:
+                    continue
+
+            # delete latest ',' if found
+            if return_data[len(return_data)-1] == ',':
+                return_data = return_data[:-1]
+            return_data += "]"
+            return_data = return_data.replace('"', "'")
+            
+        except ValueError :
+            return_data = "Format command not found"
+        except IndexError :
+            return_data = "Format command not found"
+        except:
+            return_data = "Unknown error"
             
         send_mqtt(return_data, return_topic)
         
@@ -223,7 +370,7 @@ def do_command(command, parameter_value, return_topic, set_tag=gateway_name):
             privateKey =  PrivateKey.fromPem(File.read(".ecc/privateKey.pem"))
             publicKey = privateKey.publicKey()
             
-            # ECDSA verifivcation
+            # ECDSA verification
             signatureToVerify = Signature.fromBase64(signature)
             if Ecdsa.verify(message, signatureToVerify, publicKey):
                 return_data = message.replace('"', "'")
@@ -271,29 +418,12 @@ if __name__ == "__main__":
         f.close()
         
         # if the message command format is not fulfilled, skip
-        # format must be "command/data_parameter/mqtt_return_topic"
-        # if the format is correct, parse message command as 3 parameter
+        # minimum format command ==> input_command/input_value/return_topic
         if '/' not in answer_line:
             continue
-        
-        parsing_data = answer_line.split('/')
-        
-        if len(parsing_data) != 3 and len(parsing_data) != 4:
+        if len(answer_line.split('/')) < 3:
             continue
-        
-        # Three command style
-        if len(parsing_data) == 3:
-            input_command = parsing_data[0]
-            input_parameter_value = parsing_data[1]
-            topic = parsing_data[2].strip("'")
-            specified_tag = gateway_name
 
-        # Four command style
-        if len(parsing_data) == 4:
-            input_command = parsing_data[0]
-            specified_tag = parsing_data[1]
-            input_parameter_value = parsing_data[2]
-            topic = parsing_data[3].strip("'")
-                    
         # Do message based on it command function
-        do_command(input_command, input_parameter_value, topic, specified_tag)
+        # do_command(full_msg)
+        do_command(answer_line)
